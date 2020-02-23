c917399e2a2b3c1465bd11ffa46d9b4491e9a7c6
Ishmaele <helghast50@gmail.com>
2020-02-23 04:08:46 +0300

    space

--- dupe.py
+++ dupe.py
@@ -8,7 +8,7 @@ sheet = workbook.active
 listOfElems = []
 
 for i in range(1, sheet.max_row+1):
-#    print(i, sheet.cell(row=i, column=1).value)
+#   print(i, sheet.cell(row=i, column=1).value)
     listOfElems.append(sheet.cell(row=i, column=1).value)
 
 # Create a dictionary of elements & their frequency count
